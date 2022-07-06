411f1ab96bdef3841a961103f978eef61f21e3e1
Gasser <gasser_nasser@yahoo.com>
2022-07-06 23:47:52 +0200

    ADDED THE KEYPAD

--- DIO_Basics/main.c
+++ DIO_Basics/main.c
@@ -6,19 +6,22 @@
  */
 #include <avr/io.h>
 #include <util/delay.h>
+#include "BIT_MATH.h"
 int main (void){
-	DDRB=0b00001111;
-
+	DDRA=0b00000001;
+	PORTA=0b00000010;
 	while(1){
 
-		PORTA=0b00000001;
+		if(GET_BIT(PINA,1)==0){
+			while(GET_BIT(PINA,1)==0){
 
-		for(int i=0;i<4;i++){
-			_delay_ms(500);
-			PORTB<<=1;
+			}
 
-
+			_delay_ms(10);
+			TOG_BIT(PORTA,0);
 		}
+
 	}
 	return 0;
+
 }
